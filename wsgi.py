942bd1535ae77e418e6a1ae12fa580978c53acf6
BMReda <bmredax@gmail.com>
2018-01-19 12:34:14 +0100

    Update wsgi.py
    remove alchemy

--- wsgi.py
+++ wsgi.py
@@ -2,23 +2,7 @@ from flask import Flask,jsonify
 
 from models import db, Application
 
-from sqlalchemy_jsonapi import JSONAPI
-
-
 application = Flask(__name__)
-
-# define your models classes hereafter
-POSTGRES = {
-    'user': 'myappuser',
-    'pw': 'azerty1234',
-    'db': 'sampledb',
-    'host': '172.30.145.206',
-    'port': '5432',
-}
-application.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://%(user)s:\
-%(pw)s@%(host)s:%(port)s/%(db)s' % POSTGRES
-
-db.init_app(application)
 
 @application.route("/")
 def hello():
@@ -29,9 +13,7 @@ def hello():
 @application.route("/applications")
 def applications():
     #appli_name = Application.query.filter_by(name='appli_A').first()
-    #return appli_name.name + " - " + appli_name.qsi
-    response = JSONAPI(Application).get_collection(db.session, {}, 'application')
-    return jsonify(response.data)
+    return "{ \"application\": [ { \"name\": \"appli_A\", \"qsi\": \"DPRS\" }, { \"name\": \"appli_B\", \"qsi\": \"DAL\" } ] }"
 
 
 if __name__ == "__main__":
